6f3f679db18e788e9021cc6edd5e513d5c73c22f
nprasad488 <nprasad488@gmail.com>
2022-04-18 17:28:34 +0530

    simple cpp oops code test

--- main.cpp
+++ main.cpp
@@ -2,9 +2,39 @@
 
 using namespace std;
 
+class shape{
+	public:
+		void setLength(int l){
+			length = l;	
+		}
+		void setWidth(int w){
+			width = w;
+		}
+		void setHeight(int h){
+			heigth = h;
+		}
+		
+	protected:
+		int length;
+		int width;
+		int height;
+
+};
+
+
 int main()
 {
-        cout<<"Hello World!"<<endl;
+	box Box;
+	Box.l=10;
+	Box.b=10;
+	Box.h=10;
+
+	int vol;
+
+	vol = Box.l * Box.b * Box.h;
+
+
+        cout<<"volume of box: "<<vol<<endl;
         return 0;
 }
 
